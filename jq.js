ecdc8e358b3b6e6f76d18b4a6ea009f4ca364e02
jeongwonyeong2 <wonyoung030930@gmail.com>
2022-03-15 17:09:49 +0900

    커밋

---
+++ jq.js
@@ -0,0 +1,14 @@
+const btt=$('#back-to-top');
+$(window).scroll(function(){
+    if($(window).scrollTop()>300){
+        btt.addClass('visible');
+
+    }else{
+        btt.removeClass('visible')
+    }
+})
+btt.click(function(e){
+    e.preventDefault();
+    $('html,body').animate({scrollTop:0},500)//태그 선택
+})
+
